54725d9c9e5fb809df851f462d5535eb1204de73
Vladislav Kovzov <fenix23707@gmail.com>
2021-04-25 07:26:08 +0300

    update readme

--- task02/src/main/java/text/components/Word.java
+++ task02/src/main/java/text/components/Word.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Word implements PartText {
     private String content;
     public static final String regex =
-                    "[a-zA-Z0-9-`!.-]+@\\w+\\.\\w+"//почта
+                    "[a-zA-Z0-9_.-]+@\\w+\\.\\w+"//почта
                     +"|\\+\\d{3}\\(\\d{2}\\)\\d{3}(-\\d{2}){2}"//телефон
                     +"|[^\\s\\t\\n,.:;!()\\[\\]?]+";//слово
 
